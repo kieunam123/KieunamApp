8a20e01630e37e0cf0e118a3a8cfcb09e3f058c4
KieuNamm <kieunam011@gmail.com>
2022-05-24 17:11:51 +0700

    updated api lo gin 365 microsoft

---
+++ auth/AuthConfig.ts
@@ -0,0 +1,11 @@
+export const AuthConfig = {
+    appId: '2b935271-cf4c-4517-b2c4-df2a779039b4',
+    appScopes: [
+      'openid',
+      'offline_access',
+      'profile',
+      'User.Read',
+      'MailboxSettings.Read',
+      'Calendars.ReadWrite',
+    ],
+  };
